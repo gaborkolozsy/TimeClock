64649bc1041b435058216bc5526390b0753c46fe
KolozsyGabor <kolozsygabor@gmail.com>
2017-03-11 23:36:00 +0100

    add dependencies

---
+++ TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/H2DatabaseServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.gaborkolozsy.timeclock;
+
+/**
+ *
+ * @author Kolozsy Gábor (kolozsygabor@gmail.com)
+ */
+public class H2DatabaseServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
