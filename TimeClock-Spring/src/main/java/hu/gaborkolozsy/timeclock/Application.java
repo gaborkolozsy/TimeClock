38c67c2d790bf2297ccee28f97fdd715461fb9ed
KolozsyGabor <kolozsygabor@gmail.com>
2017-03-12 19:34:34 +0100

    add application config class

--- TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/Application.java
+++ TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/Application.java
@@ -1,13 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2017, Gábor Kolozsy. All rights reserved.
+ * 
  */
 package hu.gaborkolozsy.timeclock;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
- *
+ * Maven based Time Clock application with <strong>Spring, JPA</strong> and 
+ * <strong>Hibernate</strong>.
+ * 
  * @author Kolozsy Gábor (kolozsygabor@gmail.com)
+ * @version 0.0.1-SNAPSHOT
+ * 
+ * @see org.springframework.context.ConfigurableApplicationContext
+ * @see org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class Application {
 
@@ -15,7 +23,10 @@ public class Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        try (ConfigurableApplicationContext context = 
+                new AnnotationConfigApplicationContext("hu.gaborkolozsy.timeclock.config")) {
+            
+        }
     }
     
 }
---
+++ TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017, Gábor Kolozsy. All rights reserved.
+ */
+
+package hu.gaborkolozsy.timeclock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Application configuration.
+ * 
+ * @author Kolozsy Gábor (kolozsygabor@gmail.com)
+ * @since 0.0.1-SNAPSHOT
+ * 
+ * @see org.springframework.context.annotation.ComponentScan
+ * @see org.springframework.context.annotation.Configuration
+ */
+@Configuration
+@ComponentScan("hu.gaborkolozsy.timeclock.*")
+public class ApplicationConfig {
+    
+}
