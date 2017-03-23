2ed2136b631b47563373b1f5c79f98f3f2719d2b
KolozsyGabor <kolozsygabor@gmail.com>
2017-03-23 13:26:21 +0100

    modified main

--- TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/Application.java
+++ TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/Application.java
@@ -1,19 +1,23 @@
 /*
- * Copyright (c) 2017, Gábor Kolozsy. All rights reserved.
+ * Copyright (c) 2017, Gabor Kolozsy. All rights reserved.
  * 
  */
 package hu.gaborkolozsy.timeclock;
 
+import hu.gaborkolozsy.timeclock.model.WorkingHours;
+import hu.gaborkolozsy.timeclock.service.WorkingHoursSrevice;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
- * Maven based Time Clock application with <strong>Spring, JPA</strong> and 
+ * Maven based application with <strong>Spring, JPA</strong> and 
  * <strong>Hibernate</strong> as a <strong>JPA</strong> implementation.
  * 
- * @author Kolozsy Gábor (kolozsygabor@gmail.com)
+ * @author Gabor Kolozsy (gabor.kolozsy.development@gmail.com)
  * @version 0.0.1-SNAPSHOT
- * 
  * @see ConfigurableApplicationContext
  * @see AnnotationConfigApplicationContext
  */
@@ -26,6 +30,23 @@ public class Application {
         try (ConfigurableApplicationContext context = 
                 new AnnotationConfigApplicationContext("hu.gaborkolozsy.timeclock.config")) {
             
+            WorkingHoursSrevice workTimeService = context.getBean(WorkingHoursSrevice.class);
+            //workTimeService.add();
+            workTimeService.update();
+            
+            workTimeService.updateDeveloperId(1, 3);
+            
+            WorkingHours workTime = workTimeService.findByDeveloperId(3);
+            System.out.println(workTime.getDeveloperId());
+            System.out.println(workTime.getDay());
+            System.out.println(workTime.getWorkStart());
+            System.out.println(workTime.getWorkEnd());
+            System.out.println(workTime.getAudit().getUpdatedBy());
+            
+            LocalDateTime end = workTime.getWorkEnd();
+            LocalDateTime start = workTime.getWorkStart();
+            System.out.println(LocalTime.ofSecondOfDay(end.toEpochSecond(ZoneOffset.UTC) - 
+                                                       start.toEpochSecond(ZoneOffset.UTC)));
         }
     }
     
