412d301aaef3b8e8b8eac3a47c4365cdbbc69878
KolozsyGabor <gabor.kolozsy.development@gmail.com>
2017-03-27 00:50:54 +0200

    modified

--- TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/model/abstracts/Builder.java
+++ TimeClock-Spring/src/main/java/hu/gaborkolozsy/timeclock/model/abstracts/Builder.java
@@ -8,15 +8,15 @@ package hu.gaborkolozsy.timeclock.model.abstracts;
  * Order some common methode for {@code TimeClok} entity's builder classes.
  *
  * @author Gabor Kolozsy (gabor.kolozsy.development@gmail.com)
- * @param <E> type of entity
+ * @param <T> type of entity
  * @since 0.0.1-SNAPSHOT
  */
-public interface Builder<E> {
+public interface Builder<T> {
 
     /**
      * Build the specified {@code TimeClock} entity <strong>(E)</strong>  
      * and return it.
      * @return {@code E} entity
      */
-    E build();
+    T build();
 }
