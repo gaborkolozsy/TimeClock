d038cb587cc611379c23aa9b8f23aa02acfb237a
KolozsyGabor <gabor.kolozsy.development@gmail.com>
2017-03-30 13:13:40 +0200

    modified

--- TimeClock-Spring/src/test/java/hu/gaborkolozsy/timeclock/DevelopmentTest.java
+++ TimeClock-Spring/src/test/java/hu/gaborkolozsy/timeclock/DevelopmentTest.java
@@ -8,11 +8,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 
+ * Common annotated test class.
  *
  * @author Gabor Kolozsy (gabor.kolozsy.development@gmail.com)
  * @since 0.0.1-SNAPSHOT
- * @see 
  */
 @Development
 @Transactional
