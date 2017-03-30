87831ffbeba07ff80719797c52430e38372f0f42
KolozsyGabor <kolozsygabor@gmail.com>
2017-03-30 12:52:51 +0200

    add common annotated class for test

---
+++ TimeClock-Spring/src/test/java/hu/gaborkolozsy/timeclock/DevelopmentTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017, Gabor Kolozsy. All rights reserved.
+ */
+
+package hu.gaborkolozsy.timeclock;
+
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ *
+ * @author Gabor Kolozsy (gabor.kolozsy.development@gmail.com)
+ * @since 0.0.1-SNAPSHOT
+ * @see 
+ */
+@Development
+@Transactional
+@Rollback
+public class DevelopmentTest {
+
+}
