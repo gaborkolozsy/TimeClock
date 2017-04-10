d42e06eb8b4f3bd38810ff1b2e4a8282ff67daaf
Gabor Kolozsy <gabor.kolozsy.development@gmail.com>
2017-04-10 13:13:11 +0200

    add test

---
+++ src/test/java/hu/gaborkolozsy/timeclock/controller/service/impl/WorkingHoursServiceImplIT.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017, Gabor Kolozsy. All rights reserved.
+ */
+
+package hu.gaborkolozsy.timeclock.controller.service.impl;
+
+import hu.gaborkolozsy.timeclock.DevelopmentTest;
+import hu.gaborkolozsy.timeclock.model.WorkingHours;
+import hu.gaborkolozsy.timeclock.model.WorkingHours.WorkingHoursBuilder;
+import hu.gaborkolozsy.timeclock.service.WorkingHoursService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.EntityManager;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Not working.
+ *
+ * @author Gabor Kolozsy (gabor.kolozsy.development@gmail.com)
+ * @since 0.0.1-SNAPSHOT
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class WorkingHoursServiceImplIT extends DevelopmentTest {
+
+    @Autowired
+    EntityManager em;
+    
+    @Autowired
+    @Qualifier("WorkingHoursServiceImpl")
+    WorkingHoursService workingHoursService;
+    
+    
+    /**
+     * Test is workingHoursService correct instance.
+     */
+    @Test
+    public void testCorrectType() {
+        assertThat(workingHoursService, instanceOf(WorkingHoursService.class));
+    }
+    
+    /**
+     * Test of updateWorkEnd method, of class WorkingHoursServiceImpl.
+     */
+    @Test
+    public void testUpdateWorkEnd() {
+        
+    }
+    
+    private static WorkingHours createWorkingHours() {
+        return new WorkingHoursBuilder()
+                .setDay(LocalDate.now())
+                .setWorkStart(LocalDateTime.now())
+                .build();               
+    }
+
+}
