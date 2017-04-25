65468f2b0e021b6de4bc860445b5b2e8a0affc11
Gabor Kolozsy <gabor.kolozsy.development@gmail.com>
2017-04-25 14:05:47 +0200

    modified common test annotation

--- src/test/java/hu/gaborkolozsy/timeclock/Development.java
+++ src/test/java/hu/gaborkolozsy/timeclock/Development.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextLoader;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * The common development annotation for test classes.
@@ -28,29 +29,71 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles
+@Transactional
 @ContextConfiguration
 public @interface Development {
-    
-    /**
-     * Alias for {@link #profiles}. Change active profile.
-     * @return profile's name as a string array
-     */
-    @AliasFor(annotation = ActiveProfiles.class, attribute = "profiles")
-    String[] changeProfil() default {"development"};
 
     /**
-     * Alias for {@link #classes}. Change config class.
-     * @return config class as a class array
+     * Alias for {@link #profiles}.
+     * 
+     * <p>This attribute may <strong>not</strong> be used in conjunction with
+     * {@link #profiles}, but it may be used <em>instead</em> of {@link #profiles}.
+     * @return {@code String} array with active profile(s)
      */
-    @AliasFor(annotation = ContextConfiguration.class, attribute = "classes")
-    Class<?>[] changeDefaultClass() default {ApplicationConfig.class};
+    @AliasFor(annotation = ActiveProfiles.class, attribute = "value")
+    String[] value() default {"development"};
     
     /**
-     * Alias for {@link #loader}. Change loader.
-     * @return loader class as a class array
+     * The bean definition profiles to activate.
+     * 
+     * <p>This attribute may <strong>not</strong> be used in conjunction with
+     * {@link #value}, but it may be used <i>instead</i> of {@link #value}.
+     * @return {@code String} array with active profile(s)
+     */
+    @AliasFor(annotation = ActiveProfiles.class, attribute = "profiles")
+    String[] profiles() default {"development"};
+    
+    /**
+     * Alias for {@link #xmlLocations}.
+     * 
+     * <p>This attribute may <strong>not</strong> be used in conjunction with
+     * {@link #xmlLocations}, but it may be used instead of {@link #xmlLocations}.
+     * @return {@code String} array with xml file location(s)
+     */
+    @AliasFor(annotation = ContextConfiguration.class, attribute = "value")
+    String[] xmlValue() default {};
+    
+    /**
+     * The resource locations to use for loading an
+     * {@link org.springframework.context.ApplicationContext}.
+     * 
+     * <p>This attribute may <strong>not</strong> be used in conjunction with
+     * {@link #xmlValues}, but it may be used instead of {@link #xmlValues}.
+     * @return {@code String} array with xml file location(s)
+     */
+    @AliasFor(annotation = ContextConfiguration.class, attribute = "locations")
+    String[] xmlLocations() default {};
+    
+    /**
+     * The <i>annotated classes</i> to use for loading an
+     * {@link org.springframework.context.ApplicationContext}.
+     * @return {@code Class} array with configuration class(es)
+     */ 
+    @AliasFor(annotation = ContextConfiguration.class, attribute = "classes")
+    Class<?>[] classes() default {ApplicationConfig.class};
+    
+    /**
+     * The type of {@link org.springframework.test.context.SmartContextLoader} 
+     * (or {@link ContextLoader}) to use for loading an 
+     * {@link org.springframework.context.ApplicationContext}.
+     * 
+     * <p>If not specified, the loader will be inherited from the first superclass
+     * that is annotated with <i>{@code @ContextConfiguration}</i> and specifies an
+     * explicit loader. If no class in the hierarchy specifies an explicit
+     * loader, a default loader will be used instead.
+     * @return {@code Class} array with configuration loader class
      */
     @AliasFor(annotation = ContextConfiguration.class, attribute = "loader")
-    Class<? extends ContextLoader> changeDefaultLoader() 
-            default AnnotationConfigContextLoader.class;
+    Class<? extends ContextLoader> loader() default AnnotationConfigContextLoader.class;
     
 }
